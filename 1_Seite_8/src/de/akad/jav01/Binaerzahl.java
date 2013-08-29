911f98e327b40ce57f263d021f4d1899c7100f8d
Daniel Falkner <bmw@740i.de>
2013-08-29 17:50:00 +0200

    Aufgabe 1 Seite 8

---
+++ 1_Seite_8/src/de/akad/jav01/Binaerzahl.java
@@ -0,0 +1,46 @@
+package de.akad.jav01;
+
+public class Binaerzahl {
+
+	private String string;
+	
+	public Binaerzahl(String s) {
+		
+		this.string = s;
+		
+	}
+	
+	public Binaerzahl addiere(Binaerzahl b) {
+		
+		int x = Integer.parseInt(this.string, 2);
+		int y = Integer.parseInt(b.getZahl(), 2); 
+		
+		int z = x + y;
+		
+		return new Binaerzahl(Integer.toString(z, 2));
+		
+	}
+	
+	public Binaerzahl multipliziere(Binaerzahl b) {
+		
+		int x = Integer.parseInt(this.string, 2);
+		int y = Integer.parseInt(b.getZahl(), 2); 
+		
+		int z = x * y;
+		
+		return new Binaerzahl(Integer.toString(z, 2));
+		
+	}
+	
+	public String toString() {
+		
+		return this.string;
+		
+	}
+	
+	public String getZahl() {
+		
+		return this.string;
+		
+	}
+}
