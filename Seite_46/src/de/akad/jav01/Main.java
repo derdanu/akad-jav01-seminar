f42a42c20731d185ceb4d14b097538b8313b1aa6
Daniel Falkner <bmw@740i.de>
2013-09-17 16:47:43 +0200

    Formatierungen

--- Seite_46/src/de/akad/jav01/Main.java
+++ Seite_46/src/de/akad/jav01/Main.java
@@ -1,20 +1,20 @@
 package de.akad.jav01;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		
+		Pflanze<String> p1 = new Pflanze<String>(	"Pflanze 1", 
+													Bluetezeit.FRUEHLING, 
+													"Beschreibung als String");
 		
-		Pflanze<String> p1 = new Pflanze<String>("Pflanze 1", Bluetezeit.FRUEHLING, "Beschreibung als String");
-		Pflanze<Beschreibung> p2 = new Pflanze<Beschreibung>("Pflanze 2", Bluetezeit.SOMMER, new Beschreibung("Die Kornblume gehoert zur Pflanzenart Korbbluetler", "wenig giessen"));
+		Pflanze<Beschreibung> p2 = new Pflanze<Beschreibung>(	"Pflanze 2", 
+																Bluetezeit.SOMMER, 
+																new Beschreibung("Die Kornblume gehoert zur Pflanzenart Korbbluetler", "wenig giessen")
+															);
 		
 	
 		System.out.println("Name: " + p1.getName() + "; Beschreibung: " + p1.getBeschreibung());
