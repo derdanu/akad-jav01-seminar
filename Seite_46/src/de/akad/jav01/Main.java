98c3bca309561ed1a2fd86d564d323d9f2ef3041
Daniel Falkner <bmw@740i.de>
2013-09-18 09:08:19 +0200

    direkte Pflanze ins Array

--- Seite_46/src/de/akad/jav01/Main.java
+++ Seite_46/src/de/akad/jav01/Main.java
@@ -10,21 +10,11 @@ public class Main {
 		Pflanze<String> p1 = new Pflanze<String>(	"Pflanze 1", 
 													Bluetezeit.FRUEHLING, 
 													"Beschreibung als String");
-		
-		Pflanze<Beschreibung> p2 = new Pflanze<Beschreibung>(	"Pflanze 2", 
-																Bluetezeit.SOMMER, 
-																new Beschreibung("Die Kornblume gehoert zur Pflanzenart Korbbluetler", "wenig giessen")
-															);
-		
+				
 		Pflanze<String> p3 = new Pflanze<String>(	"Pflanze 3", 
 													Bluetezeit.FRUEHLING, 
 													"Beschreibung als String");
 
-		Pflanze<Beschreibung> p4 = new Pflanze<Beschreibung>(	"Pflanze 4", 
-													Bluetezeit.SOMMER, 
-													new Beschreibung("Die ... gehoert zur Pflanzenart ...", "viel giessen")
-												);
-		
 		Pflanze<String>[] pfl = new Pflanze[2];
 		
 		pfl[0] = p1;
@@ -38,8 +28,15 @@ public class Main {
 		
 		Pflanze<Beschreibung>[] pfl2 = new Pflanze[2];
 		
-		pfl2[0] = p2;
-		pfl2[1] = p4;
+		pfl2[0] =  new Pflanze<Beschreibung>(	"Pflanze 2", 
+												Bluetezeit.SOMMER, 
+												new Beschreibung("Die Kornblume gehoert zur Pflanzenart Korbbluetler", "wenig giessen")
+											);
+		pfl2[1] = new Pflanze<Beschreibung>(	"Pflanze 4", 
+												Bluetezeit.SOMMER, 
+												new Beschreibung("Die ... gehoert zur Pflanzenart ...", "viel giessen")
+											);
+
 		
 		
 					
