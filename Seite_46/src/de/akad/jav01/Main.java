baa7f0fbba782b4387e0160adaf6908e470bb76a
Daniel Falkner <bmw@740i.de>
2013-09-18 09:00:31 +0200

    Erweitert um Array inkl Ausgabe.

--- Seite_46/src/de/akad/jav01/Main.java
+++ Seite_46/src/de/akad/jav01/Main.java
@@ -16,9 +16,39 @@ public class Main {
 																new Beschreibung("Die Kornblume gehoert zur Pflanzenart Korbbluetler", "wenig giessen")
 															);
 		
-	
-		System.out.println("Name: " + p1.getName() + "; Beschreibung: " + p1.getBeschreibung());
-		System.out.println("Name: " + p2.getName() + "; Beschreibung: " + p2.getBeschreibung().toString());
+		Pflanze<String> p3 = new Pflanze<String>(	"Pflanze 3", 
+													Bluetezeit.FRUEHLING, 
+													"Beschreibung als String");
+
+		Pflanze<Beschreibung> p4 = new Pflanze<Beschreibung>(	"Pflanze 4", 
+													Bluetezeit.SOMMER, 
+													new Beschreibung("Die ... gehoert zur Pflanzenart ...", "viel giessen")
+												);
+		
+		Pflanze<String>[] pfl = new Pflanze[2];
+		
+		pfl[0] = p1;
+		pfl[1] = p3;
+		
+		for (Pflanze<String> p : pfl) {
+			
+			System.out.println("Name: " + p.getName() + "; Beschreibung: " + p.getBeschreibung());
+			
+		}
+		
+		Pflanze<Beschreibung>[] pfl2 = new Pflanze[2];
+		
+		pfl2[0] = p2;
+		pfl2[1] = p4;
+		
+		
+				
+		for (Pflanze<Beschreibung> p : pfl2) {
+			
+			System.out.println("Name: " + p.getName() + "; Beschreibung: " + p.getBeschreibung().toString());
+			
+		}		
+		
 	
 		
 	}
